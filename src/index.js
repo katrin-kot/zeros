b780121def28444b06afe7085b163ffab8deec9b
katrin-kot <enkor89@mail.ru>
2018-09-27 20:37:37 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+  let count = 0;
+  while (number >= 5) {
+    number = Math.floor(number / 5);
+    count += number;
+  }
+  return count;
+};
